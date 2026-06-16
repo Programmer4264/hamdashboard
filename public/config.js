c6e2e81974c790b28ac36245d36286695454911f
Robert R Bleattler Jr <88990389+Programmer4264@users.noreply.github.com>
2026-06-16 13:54:57 -0400

    Update iframe URL for wind visualization in config.js

--- public/config.js
+++ public/config.js
@@ -112,8 +112,8 @@ var aIMG = [
     "weather|KPAUPPER32|__WU_API_KEY__|e", // home weather station
     "https://www.wpc.ncep.noaa.gov/noaa/noaa.gif",
     "https://www.cpc.ncep.noaa.gov/products/stratosphere/uv_index/uvi_map.gif",
-    "iframe|https://earth.nullschool.net/#current/wind/surface/level/orthographic=-73.55,39.89,3176/loc=-76.006,39.629",
     "invert|https://s.w-x.co/staticmaps/wu/wxtype/county_loc/bgm/animate.png",
+    "iframe|https://earth.nullschool.net/#current/wind/surface/level/orthographic=-73.55,39.89,3176/loc=-76.006,39.629",
     "iframe|https://www.spc.noaa.gov/exper/mesoanalysis/new/viewsector.php?sector=16&parm=pmsl&underlay=1&source=1"
     
   ],
