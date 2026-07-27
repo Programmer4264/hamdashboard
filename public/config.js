1228619794c9096f4d4ba995eabdeff59d932c2c
Robert R Bleattler Jr <88990389+Programmer4264@users.noreply.github.com>
2026-07-27 10:47:38 -0400

    Comment out WX Rundown link in config.js
    Comment out the WX Rundown YouTube link in config.js

--- public/config.js
+++ public/config.js
@@ -148,7 +148,7 @@ var aIMG = [
     "https://www.spc.noaa.gov/products/watch/validww.png", //TORNADO
     "https://www.spc.noaa.gov/products/outlook/day1otlk_1300.png", //SEVERE WEATHER
     "https://www.weather.gov/wwamap/png/US.png", //SEVERE OUTLOOK
-    "youtube|5KkmVqY_FJ4", //WX Rundown
+    //"youtube|5KkmVqY_FJ4", //WX Rundown
      "youtube|t_unhCzbolY" //Asteroid
   ],
   [
